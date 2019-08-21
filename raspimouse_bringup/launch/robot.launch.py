0b325b94944b94bc23777bdade64156a9f8c2f2e
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2019-08-22 06:21:28 +0900

    add launch file

---
+++ raspimouse_bringup/launch/robot.launch.py
@@ -0,0 +1,39 @@
+# /*******************************************************************************
+# * Copyright 2019 Yutaka Kondo
+# *
+# * Licensed under the Apache License, Version 2.0 (the "License");
+# * you may not use this file except in compliance with the License.
+# * You may obtain a copy of the License at
+# *
+# *     http://www.apache.org/licenses/LICENSE-2.0
+# *
+# * Unless required by applicable law or agreed to in writing, software
+# * distributed under the License is distributed on an "AS IS" BASIS,
+# * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# * See the License for the specific language governing permissions and
+# * limitations under the License.
+# *******************************************************************************/
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import ThisLaunchFileDir
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='raspimouse',
+            node_executable='raspimouse',
+            output='screen'),
+        Node(
+            package='rplidar_ros',
+            node_executable='rplidar_node',
+            output='screen'),
+    ])
