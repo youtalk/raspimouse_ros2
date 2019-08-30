78424b81e00a2e37738ff3fac3eb6e43dffba025
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2019-08-30 14:48:14 +0900

    enable use_pulse_counters

--- raspimouse_bringup/launch/robot.launch.py
+++ raspimouse_bringup/launch/robot.launch.py
@@ -37,6 +37,8 @@ def generate_launch_description():
             package='raspimouse',
             node_executable='raspimouse',
             node_name='raspimouse_driver',
+            parameters=[{'use_pulse_counters': True,
+                         'use_light_sensors': True}]),
             output='screen'),
         Node(
             package='rplidar_ros',
